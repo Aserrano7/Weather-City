8a0c109d95e700a41c3449d113aa046bb988f51a
Aserrano7 <serranoandres022@gmail.com>
2023-11-13 10:45:33 -0800

    added fetch url to get weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,23 @@ $("button").on("click", function (event) {
     cities.push(city)
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
         city + "&units=imperial&appid=" + APIKey;
+        fetch(queryUrl)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            var currentIcon = data.weather[0].icon;
+            var currentTemp = parseInt(data.main.temp) + "°F";
+            var currentWind = parseInt(data.wind.speed) + "mph";
+            var currentHum = data.main.humidity + "%";
+            var currentIconURL = "http://openweathermap.org/img/w/" + currentIcon + ".png";
+
+            $("#city").text(city);
+            $("#temp").text("Temp: " + currentTemp);
+            $("#humidity").text("Humidity: " + currentHum);
+            $("#wind").text('Wind: ' + currentWind);
+            $("#weatherIcon").attr("src", currentIconURL);
+
+})
 })
