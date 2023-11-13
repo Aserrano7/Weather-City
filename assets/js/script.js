d00c941f760d01716b5aaef80f49338261795a28
Aserrano7 <serranoandres022@gmail.com>
2023-11-13 10:55:44 -0800

    added fetch url for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $("button").on("click", function (event) {
     cities.push(city)
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" +
         city + "&units=imperial&appid=" + APIKey;
-        fetch(queryUrl)
+    fetch(queryUrl)
         .then(function (response) {
             console.log(response);
             return response.json();
@@ -25,5 +25,36 @@ $("button").on("click", function (event) {
             $("#wind").text('Wind: ' + currentWind);
             $("#weatherIcon").attr("src", currentIconURL);
 
-})
-})
+            var url2 =
+                "https://api.openweathermap.org/data/2.5/forecast?q=" + city + '&units=imperial&appid=' + APIKey;
+
+      
+            fetch(url2)
+                .then(function (response) {
+                    console.log(response);
+                    return response.json();
+                })
+                .then(function (data) {
+                    var date = new Date();
+                    for (i = 0; i < 6; i++) {
+                        var val = (date.getMonth() + 1) + "/" + (date.getDate() + i) + "/" + date.getFullYear();
+                        $("#day-" + i).text(val);
+                        var icons = [];
+                        var iconsURL = [];
+                        icons[i] = data.list[i + 1].weather[0].icon;
+                        iconsURL[i] = "http://openweathermap.org/img/w/" + icons[i] + ".png";
+                        $("#icon-" + i).attr("src", iconsURL[i]);
+                        var temperture = parseInt(data.list[i + 1].main.temp) + "°F";
+                        $('#temp-' + i).text("Temp: " + temperture);
+                        var humid = data.list[i + 1].main.humidity + "%";
+                        $('#humidity-' + i).text("Humidity: " + humid);
+                        var windSpeed = parseInt(data.list[i + 1].wind.speed) + "mph";
+                        $("#wind-" + i).text('Wind: ' + windSpeed);
+
+                    }
+
+                });
+        });
+
+});
+console.log(cities);
